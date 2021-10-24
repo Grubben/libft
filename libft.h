cd26cb5fa0ca8afcbbb27fa2eed4e3170b7b8a3b
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s4.42lisboa.com>
2021-10-24 13:32:41 +0100

    libft: updated to include all core funcs

--- libft.h
+++ libft.h
@@ -41,3 +41,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+int		ft_atoi(const char *str);
