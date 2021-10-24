5cc3749111e5c7cbdeec02bb8c2cc085b86c9724
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r8s1.42lisboa.com>
2021-10-24 11:05:53 +0100

    ft_strnstr: finished but the len param might not be working perfectly

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,19 +6,29 @@ if result is 0 means they are the same there and that first letter is indeed suc
 if not continue until final '\0'
 */
 
-char    *strnstr(const char *haystack, const char *needle, size_t len)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t  i;
 
     i = 0;
-    while (haystack[i] != '\0')
+    while (haystack[i] != '\0' && i < len)
     {
         if (haystack[i] == needle[0])
         {
             if (ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0)
-                return (haystack + i);
+                return ((char *)haystack + i);
         }
         i++;
     }
     return (NULL);
+}
+
+int	main(void)
+{
+	char	BIG[] = "goodbye";
+	char	SMALL[] = "d";
+	size_t	SIZE = 7;
+
+	printf("%s\n", strnstr(BIG, SMALL, SIZE));
+	printf("%s\n", ft_strnstr(BIG, SMALL, SIZE));
 }
