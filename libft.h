92ceaad34aebcb3c807db3c60cb4fd3c0010cf09
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s4.42lisboa.com>
2021-10-24 11:55:31 +0100

    ft_isprint: added, solved and normed

--- libft.h
+++ libft.h
@@ -4,25 +4,29 @@
 #include <string.h>
 #include <ctype.h>
 
-void    ft_bzero(void *s, size_t n);
+int		ft_isalpha(char c);
 
 int		ft_isalnum(char c);
 
-int		ft_isalpha(char c);
+int		ft_isdigit(char c);
 
 int		ft_isascii(char c);
 
-int		ft_isdigit(char c);
+int		ft_isprint(int c);
 
 size_t	ft_strlen(const char *s);
+
+void	*ft_memset(void *b, int c, size_t len);
+
+void    ft_bzero(void *s, size_t n);
 
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
-void	*ft_memset(void *b, int c, size_t len);
-
 size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 int		ft_toupper(int c);
 
@@ -35,3 +39,5 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
