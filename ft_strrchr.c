65595f5add8432e0e8d2e2209542d44dc54779ec
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s2.42lisboa.com>
2021-10-28 11:36:43 +0100

    ft_strrchr: fixed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,12 +6,21 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 12:31:47 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/10/22 12:34:38 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/10/28 11:36:13 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+The strchr() function locates the first occurrence
+	of c (converted to a char) in the string pointed to by s.
+	The terminating null character is considered to be part of the
+    string; therefore if c is `\0', the functions locate the terminating `\0'.
+
+The strrchr() function is identical to strchr(), except it
+	locates the last occurrence of c.
+*/
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	len;
@@ -21,7 +30,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = len;
 	while (i < len + 1)
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)s + i);
 		i--;
 	}
