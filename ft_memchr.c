d5644b59d5058e7ab5c9f3fd5ac6e8f02b4ce460
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s2.42lisboa.com>
2021-10-28 11:22:16 +0100

    ft_memchr: fixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,12 +6,15 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 12:55:04 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/10/24 11:25:39 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/10/28 11:21:38 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+The memchr() function locates the first occurrence of c (converted to an unsigned char) in string s.
+*/
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
@@ -19,9 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (((char *) s)[i] == '\0')
-			return (NULL);
-		if (((char *) s)[i] == c)
+		if (((char *) s)[i] == (unsigned char)c)
 			return ((char *)s + i);
 		i++;
 	}
