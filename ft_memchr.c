670294372ce615da1ae3d5a63da70b76fa966048
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s2.42lisboa.com>
2021-10-28 11:31:24 +0100

    ft_memcmp: fixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,15 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 12:55:04 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/10/28 11:21:38 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/10/28 11:29:46 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-The memchr() function locates the first occurrence of c (converted to an unsigned char) in string s.
+The memchr() function locates the first occurrence
+	of c (converted to an unsigned char) in string s.
 */
 void	*ft_memchr(const void *s, int c, size_t n)
 {
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,26 +6,32 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 11:27:00 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/10/28 11:07:54 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/10/28 11:30:46 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-The memcmp() function compares byte string s1 against byte string s2.  Both strings are assumed to be n bytes long.
+The memcmp() function compares
+	byte string s1 against byte string s2.
+Both strings are assumed to be n bytes long.
 */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	char	*sone;
+	char	*stwo;
 	size_t	i;
 
-	if (n == 0 || (ft_strlen(s1) == 0 || ft_strlen(s2) == 0))
+	if (n == 0)
 		return (0);
+	sone = (char *)s1;
+	stwo = (char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if (((char *)s1)[i] != ((char *)s2)[i])
-			return (((char *)s1)[i] - ((char *)s2)[i]);
+		if (sone[i] != stwo[i])
+			return ((unsigned char)sone[i] - (unsigned char)stwo[i]);
 		i++;
 	}
 	return (0);
