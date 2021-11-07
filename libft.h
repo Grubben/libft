701098a3beb8a658969d91764d00618847049ded
Grubben <antoniomsprc@gmail.com>
2021-11-07 18:32:23 +0000

    libft: added bonus section

--- libft.h
+++ libft.h
@@ -84,6 +84,14 @@ void	ft_putendl_fd(char *s, int fd);
 
 void	ft_putnbr_fd(int n, int fd);
 
+/* BONUS */
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+
 /* MINE */
 int		ft_charinside(char c, const char *s);
 
