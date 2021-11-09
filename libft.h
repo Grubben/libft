e5c58ace83860627d24e109119126daabe95366a
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r11s2.42lisboa.com>
2021-11-09 11:28:23 +0000

    ft_lstlast: started, finished and normed. But unchecked

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 19:39:23 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/11/09 11:16:34 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/11/09 11:24:50 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 
 int		ft_lstsize(t_list *lst);
 
-t_list	ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 /* MINE */
 int		ft_charinside(char c, const char *s);
