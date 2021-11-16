40e906e6e1e3b3b8b1a2e2c3b0149e253d4e643c
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r14s3.42lisboa.com>
2021-11-16 12:26:33 +0000

    ft_itoa: prepped for final entrega

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,68 +6,69 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 10:28:59 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/11/16 12:19:10 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:25:39 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// static void	ft_reversen(char *s, unsigned int n)
-// {
-// 	char	c;
-// 	int		i;
-// 	int		j;
+static void	ft_reversen(char *s, unsigned int n)
+{
+	char	c;
+	int		i;
+	int		j;
 
-// 	if (n < 2)
-// 		return ;
-// 	j = n - 1;
-// 	if (n > ft_strlen(s))
-// 		j = ft_strlen(s) - 1;
-// 	i = 0;
-// 	while (i < j)
-// 	{
-// 		c = s[i];
-// 		s[i] = s[j];
-// 		s[j] = c;
-// 		i++;
-// 		j--;
-// 	}
-// }
+	if (n < 2)
+		return ;
+	j = n - 1;
+	if (n > ft_strlen(s))
+		j = ft_strlen(s) - 1;
+	i = 0;
+	while (i < j)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+		i++;
+		j--;
+	}
+}
 
 /*
 	1) make 12 places: the minus, each digit and the '\0'
 
 */
 
-// char	*ft_itoa(int n)
-// {
-// 	char			digit[12];
-// 	char			*final;
-// 	signed char		sign;
-// 	unsigned int	new;
+char	*ft_itoa(int n)
+{
+	char			digit[12];
+	char			*final;
+	signed char		sign;
+	unsigned int	new;
 
-// 	sign = 1;
-// 	if (n < 0)
-// 		sign = -1;
-// 	new = n * sign;
-// 	ft_memset(digit, 0, 12);
-// 	n = 0;
-// 	if (new == 0)
-// 		digit[n++] = 0 + '0';
-// 	while (new != 0)
-// 	{
-// 		digit[n++] = new % 10 + '0';
-// 		new = new / 10;
-// 	}
-// 	if (sign < 0)
-// 		digit[n] = '-';
-// 	ft_reversen(digit, n + 1);
-// 	final = ft_strdup(digit);
-// 	if (!final)
-// 		return (NULL);
-// 	return (final);
-// }
+	sign = 1;
+	if (n < 0)
+		sign = -1;
+	new = n * sign;
+	ft_memset(digit, 0, 12);
+	n = 0;
+	if (new == 0)
+		digit[n++] = 0 + '0';
+	while (new != 0)
+	{
+		digit[n++] = new % 10 + '0';
+		new = new / 10;
+	}
+	if (sign < 0)
+		digit[n] = '-';
+	ft_reversen(digit, n + 1);
+	final = ft_strdup(digit);
+	if (!final)
+		return (NULL);
+	return (final);
+}
 
+/*
 void	aux(char *s, long int n)
 {
 	static unsigned int	i;
@@ -94,8 +95,9 @@ char	*ft_itoa(int n)
 	aux(digit, n);
 	return (ft_strdup(digit));
 }
+*/
 
-// /*
+/*
 int	main(void)
 {
 
@@ -107,4 +109,4 @@ int	main(void)
 
 	return (0);
 }
-// */
+*/
